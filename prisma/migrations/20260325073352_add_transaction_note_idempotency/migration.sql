3506700b283c269521a079a572d11d3f97fe4b6f
dhyeytank123 <dhyey.tank@drcsystems.com>
2026-03-25 13:04:29 +0530

    Add idempotency key in Transaction schema

---
+++ prisma/migrations/20260325073352_add_transaction_note_idempotency/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - The `status` column on the `Transaction` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- AlterTable
+ALTER TABLE "Transaction" ADD COLUMN     "note" TEXT,
+DROP COLUMN "status",
+ADD COLUMN     "status" "TransactionStatus" NOT NULL DEFAULT 'PENDING',
+ALTER COLUMN "idempotencyKey" DROP NOT NULL;
